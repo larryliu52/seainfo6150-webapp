d5c753cc0956349b13ea2f11bdfa9e29e241e9bf
Larryliu52 <lanzeliu52@gmail.com>
2020-10-18 14:54:27 -0700

    Assignment 6

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ArticleListItem from '../ArticleListItem/ArticleListItem';
 
 const ArticleList = (props) => {
-    const list = props.list
-    const mapList = list.map((listItem) => (
-        <li><ArticleListItem article = {listItem}/></li>
-    ))
+  const list = props.list
+  const mapList = list.map((listItem) => (
+      <li><ArticleListItem article = {listItem}/></li>))
+  
   return (
     <article>
-        <ul>{mapList}</ul>
+      <ul>{mapList}</ul>
     </article>
   );
 };
